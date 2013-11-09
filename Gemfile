7d8c7b1602f784c51b1f2bade343a07132673ed6
Markov Alexey <piton4eg@mail.ru>
2013-11-09 12:59:31 +0400

    init

---
+++ Gemfile
@@ -0,0 +1,34 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.0'
+
+gem 'sqlite3'
+
+group :assets do
+  gem 'sass-rails', '~> 4.0.0'
+  gem 'uglifier', '>= 1.3.0'
+  gem 'coffee-rails', '~> 4.0.0'
+
+end
+gem "angularjs-rails", "~> 1.2.0"
+gem 'jquery-rails'
+
+gem 'turbolinks'
+
+gem 'jbuilder', '~> 1.2'
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
+# Use ActiveModel has_secure_password
+# gem 'bcrypt-ruby', '~> 3.0.0'
+
+# Use unicorn as the app server
+# gem 'unicorn'
+
+# Use Capistrano for deployment
+# gem 'capistrano', group: :development
+
+# Use debugger
+# gem 'debugger', group: [:development, :test]
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Raffler::Application.config.secret_key_base = '71530ec6d9efcbf8d1baebcefc2bbbecfe7615276e599ede172359f8318bd116824c7e4a60f0d575b581cd3e8d0f22a8d347fcda06058c4620df51bd77a88261'
